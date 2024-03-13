034742f0492e9186b80aed399bc3bcee0531e698
pankaj jain <pj9009815223@gmail.com>
2024-03-13 17:54:08 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItmeListComponent } from './itme-list/itme-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ItmeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/itme-list/itme-list.component.ts
+++ src/app/itme-list/itme-list.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-itme-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './itme-list.component.html',
   styleUrl: './itme-list.component.css'
 })
 export class ItmeListComponent {
 
+      name="pankaj"
+      lastname = "jain"
+      mobileNo = 9522189879
+      isbtn = false
+      inputval = "text"
+
+     users = [
+      {name:"pankaj",status:true},
+      {name:"shubham",status:false},
+      {name:"neha",status:true}
+     ]
+
+     handleButtonClick(): void {
+      console.log('Button clicked!');
+    }
+
+    pageTitle: string = "Angular Data Binding Practice";
+  a: number = 5;
+  b: number = 7;
+  disable: boolean = false;
+
+  toggleChange(): void {
+    this.disable = !this.disable;
+  }
+
+  sum(): number {
+    return this.a + this.b;
+  }
+
+  sumPlusTen(): number {
+    return this.sum() + 10;
+  }
+
+  imageUrl: string = "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600";
+
+
+  isError: boolean = false;
 }
