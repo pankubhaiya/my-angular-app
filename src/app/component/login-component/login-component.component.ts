44a84bf5e714187a1d71f2b6043204a091c6b9a9
pankaj jain <pj9009815223@gmail.com>
2024-03-18 14:18:29 +0530

    done-login-signup

--- src/app/component/login-component/login-component.component.ts
+++ src/app/component/login-component/login-component.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
 export class LoginComponentComponent {
-
+  
+  login(email:string,password:string) {
+    // Implement login logic here
+    console.log('Login form submitted',email,password);
+  }
 }
--- src/app/component/signup-component/signup-component.component.ts
+++ src/app/component/signup-component/signup-component.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './signup-component.component.html',
   styleUrl: './signup-component.component.css'
 })
 export class SignupComponentComponent {
-
+  register(email:string,password:string) {
+    // Implement register logic here
+    console.log('register form submitted',email,password);
+  }
 }
