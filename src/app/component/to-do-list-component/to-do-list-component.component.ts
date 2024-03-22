26525f1138a8a80fe421fe6680befaa27f90a882
pankaj jain <pj9009815223@gmail.com>
2024-03-23 01:54:30 +0530

    udate todo

--- src/app/component/to-do-list-component/to-do-list-component.component.ts
+++ src/app/component/to-do-list-component/to-do-list-component.component.ts
@@ -11,17 +11,51 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './to-do-list-component.component.css'
 })
 export class ToDoListComponentComponent {
+  todos: Todo[] = [];
+  todoTitle: string = '';
+  todoDescription: string = '';
+  isUpdateMode: boolean = false;
+  updateTodoId: number | null = null;
 
-  todos: Todo[] = [
-    { title: 'Task 1', description: 'Description for Task 1', completed: false },
-    { title: 'Task 2', description: 'Description for Task 2', completed: true }
-  ];
-  addTodo(title: string, description: string) {
-    this.todos.push(new Todo(title, description, false));
+  addOrUpdateTodo() {
+    if (this.isUpdateMode && this.updateTodoId !== null) {
+      const index = this.todos.findIndex(todo => todo.id === this.updateTodoId);
+      if (index !== -1) {
+        this.todos[index].title = this.todoTitle;
+        this.todos[index].description = this.todoDescription;
+        this.isUpdateMode = false;
+        this.updateTodoId = null;
+      }
+    } else {
+      // Add new todo
+      this.todos.push(new Todo(
+        this.todos.length + 1,
+        this.todoTitle,
+        this.todoDescription,
+        false
+      ));
+    }
+    // Clear input fields
+    this.todoTitle = '';
+    this.todoDescription = '';
   }
-  constructor() { }
 
   toggleCompletion(todo: Todo) {
     todo.completed = !todo.completed;
+  }
+
+  setUpdateMode(todo: Todo) {
+    // Set values of input fields to the selected todo item's title and description
+    this.todoTitle = todo.title;
+    this.todoDescription = todo.description;
+    this.isUpdateMode = true;
+    this.updateTodoId = todo.id;
+  }
+
+  deleteTodo(todoId: number) {
+    const index = this.todos.findIndex(todo => todo.id === todoId);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
   }
 }
--- src/app/component/to-do-list-component/todo.ts
+++ src/app/component/to-do-list-component/todo.ts
@@ -1,6 +1,7 @@
 // todo.model.ts
 export class Todo {
     constructor(
+      public id: number,
       public title: string,
       public description: string,
       public completed: boolean
